c6d6191e6076d01f6bf58e505c8e2d99b86a00ca
gongbing123 <834718027@qq.com>
2018-11-06 16:44:20 +0800

    app create

---
+++ www/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+import logging;logging.basicConfig(level=logging.INFO)
+import asyncio
+from aiohttp import web
+
+
+def index(request):
+    return web.Response(body=b'<h1>hello,world</h1>',content_type='text/html')
+
+async def init(loop):
+    app=web.Application(loop=loop)
+    app.router.add_route('GET','/',index)
+    srv=await loop.create_server(app._make_handler(),'127.0.0.1',9000)
+    logging.info('server started at 127.0.0.1:9000...')
+    return srv
+
+loop=asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
